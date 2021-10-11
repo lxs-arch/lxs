fba458c3082d3bfc4c9e4c04439fe61955780889
雪山 刘 <2112852325@qq.com>
2021-10-11 16:39:05 +0800

    杨辉三角

--- 杨辉三角/test.c
+++ 杨辉三角/test.c
@@ -1,26 +1,21 @@
 #define _CRT_SECURE_NO_WARNINGS 1
+//实现strcpy函数
 #include <stdio.h>
+#include<assert.h>
+char* my_strcpy(char* s1,const char* s2) {
+	assert(s1 && s2);
+	char* ret = s1;
+	while (*s1++ = *s2++;) {
+		
+		;
+	}
+	return ret;
+}
 int main() {
-	int n;//包含6行数据
-	scanf("%d", &n);
-	int i, j, a[30][30] = { 0 };
-	for (i = 0; i < n; i++) {
-		for (j = 0; j < n; j++) {
-			a[i][0] = 1;
-			a[i][i] = 1;
-		}
-	}
-	for (i = 2; i < n; i++) {
-		for (j = 1; j <= i - 1; j++) {
-			a[i][j] = a[i - 1][j] + a[i - 1][j - 1];
-		}
-	}
-	for (i = 0; i < n; i++) {
-		for (j = 0; j <= i; j++) {
-			printf("%5d ", a[i][j]);
-		}
-		printf("\n");
-	}
+	char arr1[10] = "abc";
+	char arr2[] = "edf";
+	my_strcpy(arr1, arr2);//把arr2复制到arr1中
+	printf("%s", arr1);
 	return 0;
 }
 
