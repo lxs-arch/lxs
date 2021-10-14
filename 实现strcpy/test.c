ed9fc4b12f33390f499901090eaeb640be94abcc
雪山 刘 <2112852325@qq.com>
2021-10-14 20:51:57 +0800

    实现strcpy/test.c

--- 实现strcpy/test.c
+++ 实现strcpy/test.c
@@ -1,20 +1,25 @@
 #define _CRT_SECURE_NO_WARNINGS 1
 //实现strcpy函数
-#include <stdio.h>
+#include<stdio.h>
+#include<string.h>
 #include<assert.h>
-char* my_strcpy(char* s1, const char* s2) {
-	assert(s1 && s2);
-	char* ret = s1;
-	while (*s1++ = *s2++;) {
-
-		;
-	}
-	return ret;
+char* my_strcpy(char* str1, char* str2)
+{
+    assert(str2);//判断源地址是否为空；
+    char* cur = str1;
+    while (*str2 != '\0')
+    {
+        *str1 = *str2;
+        str1++;
+        str2++;
+    }
+    *str1 = '\0';
+    return cur;
 }
-int main() {
-	char arr1[10] = "abc";
-	char arr2[] = "edf";
-	my_strcpy(arr1, arr2);//把arr2复制到arr1中
-	printf("%s", arr1);
-	return 0;
+int main()
+{
+    char str1[10];
+    char str2[10] = "China";
+    printf("%s\n", my_strcpy(str1, str2));
+    return 0;
 }
